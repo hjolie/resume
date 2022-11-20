00e735a5e4469db0ab02ccc5a16a76ce9a9add59
hjolie <huang_jolie@outlook.com>
2022-11-20 13:17:47 +0800

    4th commit

--- script.js
+++ script.js
@@ -1,8 +1,59 @@
+// Hamburger Menu
+const sideMenu = document.getElementById('side-menu');
+const openIcon = document.getElementById('open-icon');
+const closeIcon = document.getElementById('close-icon');
+const links = document.querySelectorAll('.link');
+
+openIcon.addEventListener('click', () => {
+    // sideMenu.classList.add('show-menu');
+    sideMenu.style.left = "0";
+
+    openIcon.classList.remove('fixed-open-icon');
+
+    links.forEach(link => {
+        link.classList.add('side-menu-link');
+    })
+
+    const sideMenuLinks = document.querySelectorAll('.side-menu-link');
+    sideMenuLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            sideMenu.style.left = "-250px";
+        })
+    })
+})
+
+closeIcon.addEventListener('click', () => {
+    // sideMenu.classList.remove('show-menu');
+    sideMenu.style.left = "-250px";
+    openIcon.classList.add('fixed-open-icon');
+})
+
+
 // Go Top Button
 const goTop = document.querySelector('.go-top');
 
 window.addEventListener('scroll', () => {
     const scrollHeight = window.pageYOffset;
+
+    
+    if (scrollHeight > 500 && sideMenu.style.left !== "0") {
+        openIcon.classList.add('fixed-open-icon');
+    }
+    else {
+        openIcon.classList.remove('fixed-open-icon');
+    }
+    
+
+    // if (window.matchMedia('(max-width: 1310px)').matches) {
+    //     if (sideMenu.style.left === "0") {
+    //         openIcon.classList.remove('fixed-open-icon');
+    //     } else if (scrollHeight > 500) {
+    //         openIcon.classList.add('fixed-open-icon');
+    //     }
+    //     else {
+    //         openIcon.classList.remove('fixed-open-icon');
+    //     }
+    // }
 
     if (scrollHeight > 500) {
         goTop.classList.add('show-go-top');
@@ -11,21 +62,6 @@ window.addEventListener('scroll', () => {
     }
 })
 
-
-// Hamburger Menu
-const sideMenu = document.getElementById('side-menu');
-const openIcon = document.getElementById('open-icon');
-const closeIcon = document.getElementById('close-icon');
-
-openIcon.addEventListener('click', () => {
-    // sideMenu.classList.add('show-menu');
-    sideMenu.style.left = "0";
-})
-
-closeIcon.addEventListener('click', () => {
-    // sideMenu.classList.remove('show-menu');
-    sideMenu.style.left = "-250px";
-})
 
 // window.addEventListener('click', (e) => {
 //     if (e.target.classList.contains('fa-bars')) {
